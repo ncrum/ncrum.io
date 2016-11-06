1c19f401fcc1ed3580af5b0e9fbfcc211623cb69
Nick Crum <ncrum@broadleafcommerce.com>
2016-11-05 23:59:51 -0500

    adjusting production properties

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -28,7 +28,6 @@ module.exports = {
             {
                 test: /\.js$/,
                 loaders: ['babel'],
-                exclude: [path.join(__dirname, 'node_modules')],
             }
         ],
     },
