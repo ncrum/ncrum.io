262c9f8f059c03a73b747a2fa01a7c6e513e933a
Nick Crum <ncrum@broadleafcommerce.com>
2016-11-06 00:48:16 -0500

    fixed styling

--- src/server/server.js
+++ src/server/server.js
@@ -7,6 +7,7 @@ let app = express()
 app.use(compression())
 app.use('/static', express.static(path.join(__dirname, 'static')))
 app.use('/fonts', express.static(path.join(__dirname, 'static/fonts')))
+app.use('/style', express.static(path.join(__dirname, 'static/style')))
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'static/index.html'))
